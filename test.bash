dcbd931bec704ff0e88b9eb675712d42db57e323
basiliskv <subarickv4.1@gmail.com>
2023-11-11 18:58:11 +0900

    test.bashを修正

--- test.bash
+++ test.bash
@@ -11,7 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+[ "${out}" = "1+2+3+4+5+=15" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK	# &&（AND記号）は左側が成功すると右側を実行
 exit $res
