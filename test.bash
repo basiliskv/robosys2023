0b1108c00b2cc077807edc84c70dbe025dd6ec1d
basiliskv <subarickv4.1@gmail.com>
2023-11-12 21:05:48 +0900

    test.bashに実行後に数値を追加する場合のテストを追加

--- test.bash
+++ test.bash
@@ -13,5 +13,8 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = "1+2+3+4+5=15" ] || ng ${LINENO}
 
+out=$(echo -e "1\n2\n3\n4\n5\n6\n7\n8\n9\n" | ./plus)  #実行後に数値を入力しても正しく動作するかテスト
+[ "${out}" = "1+2+3+4+5+6+7+8+9=45" ] || ng ${LINENO}
+
 [ "$res" = 0 ] && echo OK	# &&（AND記号）は左側が成功すると右側を実行
 exit $res
